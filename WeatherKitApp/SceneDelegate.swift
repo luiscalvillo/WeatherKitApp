6bb4f70317966fa4109ff0953912eefa0e9d3940
Luis Calvillo <luiscalvillo89@gmail.com>
2024-10-20 22:46:58 -0700

    Remove tab bar

--- WeatherKitApp/SceneDelegate.swift
+++ WeatherKitApp/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        let vc = TabBarController()
+        let vc = WeatherVC()
         let window = UIWindow(windowScene: windowScene)
         window.rootViewController = vc
         window.makeKeyAndVisible()
